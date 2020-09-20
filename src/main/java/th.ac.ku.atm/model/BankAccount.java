60d2feb6d28dc3832fb0d1ff545eb127b4543928
Thanachot Kokklang <thanachot.ko@ku.th>
2020-09-20 21:36:04 +1200

    Initial commit

---
+++ src/main/java/th.ac.ku.atm/model/BankAccount.java
@@ -0,0 +1,49 @@
+package th.ac.ku.atm.model;
+
+public class BankAccount {
+    private int id;
+    private int customerId;
+    private String type;
+    private double balance;
+
+    public BankAccount(int id, int customerId, String type, double balance) {
+        this.id = id;
+        this.customerId = customerId;
+        this.type = type;
+        this.balance = balance;
+    }
+
+    public int getIdBank() {
+        return id;
+    }
+
+    public int getCustomerIdBank() {
+        return customerId;
+    }
+
+    public String getTypeBank() {
+        return type;
+    }
+
+    public double getBalanceBank() {
+        return balance;
+    }
+
+    public void setBalanceBank(double balance) {
+        this.balance = balance;
+    }
+
+    public void setIdBank(int id) {
+        this.id = id;
+    }
+
+    public void setCustomerIdBank(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public void setTypeBank(String type) {
+        this.type = type;
+    }
+
+
+}
---
+++ src/main/java/th.ac.ku.atm/model/Customer.java
@@ -0,0 +1,40 @@
+package th.ac.ku.atm.model;
+
+public class Customer {
+    private int id;
+    private String name;
+    private String pin;
+
+    public Customer(int id, String name, String pin) {
+        this.id = id;
+        this.name = name;
+        this.pin = pin;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPin(String pin) {
+        this.pin = pin;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPin() {
+        return pin;
+    }
+}
+
+
+
